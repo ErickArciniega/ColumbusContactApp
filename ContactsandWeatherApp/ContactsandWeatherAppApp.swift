e3d3310c5d9570d0e23ae9bede9f7315b197833d
Erick Arciniega <arciniegaerick@gmail.com>
2022-04-25 18:53:25 -0500

    first commit

---
+++ ContactsandWeatherApp/ContactsandWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsandWeatherAppApp.swift
+//  ContactsandWeatherApp
+//
+//  Created by Erick Gonzalez Arciniega on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactsandWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
